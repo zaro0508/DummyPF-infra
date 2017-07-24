dc397b393241fc92832a278a1e7b05a003fe86f3
Khai Do <zaro0508@gmail.com>
2017-07-24 10:22:18 -0700

    move script inot deploy_infra.sh

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+set -ex
+
+# validate cf templates (local)
+aws cloudformation validate-template --template-body file://cf_templates/eb_bridgepf_bootstrap.yml
+aws cloudformation validate-template --template-body file://cf_templates/eb_bridgepf.yml
+
+# validate cf templates (from s3 bucket, required when template files are greater than 50K)
+# https://docs.aws.amazon.com/cli/latest/reference/cloudformation/create-stack.html
+#  - aws s3 cp cf_templates s3://org-sagebridge-bridgepf-artifacts-sandbox-infra-develop/cf_templates --recursive
+#  - aws cloudformation validate-template --template-url https://s3.amazonaws.com/org-sagebridge-bridgepf-artifacts-sandbox-infra-develop/cf_templates/eb_bridgepf_bootstrap.yml
+#  - aws cloudformation validate-template --template-url https://s3.amazonaws.com/org-sagebridge-bridgepf-artifacts-sandbox-infra-develop/cf_templates/eb_bridgepf.yml
+
+# Handle message that shouldn't be an error, https://github.com/hashicorp/terraform/issues/5653 
 message=$(./update_cf_stack.sh 2>&1 1>/dev/null)
 error_code=$(echo $?)
 if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
