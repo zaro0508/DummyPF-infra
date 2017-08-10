d7de03c7eba0b4a688e7987e944c7756f6e9115d
Khai Do <zaro0508@gmail.com>
2017-08-10 15:11:37 -0700

    pass vars straight from travis

--- update_cf_stack.sh
+++ update_cf_stack.sh
@@ -1,80 +1,46 @@
 #!/usr/bin/env bash
-
-# evaluate vars
-eval export "AppDeployBucket=\$AppDeployBucket_$TRAVIS_BRANCH"
-eval export "AwsKeyUpload=\$AwsKeyUpload_$TRAVIS_BRANCH"
-eval export "AwsKeyUploadCms=\$AwsKeyUploadCms_$TRAVIS_BRANCH"
-eval export "AwsSecretKeyConsents=\$AwsSecretKeyConsents_$TRAVIS_BRANCH"
-eval export "AwsSecretKeyUpload=\$AwsSecretKeyUpload_$TRAVIS_BRANCH"
-eval export "AwsSecretKeyUploadCms=\$AwsSecretKeyUploadCms_$TRAVIS_BRANCH"
-eval export "AwsSnsKey=\$AwsSnsKey_$TRAVIS_BRANCH"
-eval export "AwsSnsSecretKey=\$AwsSnsSecretKey_$TRAVIS_BRANCH"
-eval export "BridgeEnv=\$BridgeEnv_$TRAVIS_BRANCH"
-eval export "BridgeHealthcodeKey=\$BridgeHealthcodeKey_$TRAVIS_BRANCH"
-eval export "EmailUnsubscribeToken=\$EmailUnsubscribeToken_$TRAVIS_BRANCH"
-eval export "EnterpriseStormpathHref=\$EnterpriseStormpathHref_$TRAVIS_BRANCH"
-eval export "EnterpriseStormpathId=\$EnterpriseStormpathId_$TRAVIS_BRANCH"
-eval export "EnterpriseStormpathSecret=\$EnterpriseStormpathSecret_$TRAVIS_BRANCH"
-eval export "HibernateConnectionPassword=\$HibernateConnectionPassword_$TRAVIS_BRANCH"
-eval export "HibernateConnectionUrl=\$HibernateConnectionUrl_$TRAVIS_BRANCH"
-eval export "HibernateConnectionUsername=\$HibernateConnectionUsername_$TRAVIS_BRANCH"
-eval export "RedisUrl=\$RedisUrl_$TRAVIS_BRANCH"
-eval export "SSLCertArn=\$SSLCertArn_$TRAVIS_BRANCH"
-eval export "SynapseApiKey=\$SynapseApiKey_$TRAVIS_BRANCH"
-eval export "SynapseUser=\$SynapseUser_$TRAVIS_BRANCH"
-eval export "SysopsEmail=\$SysopsEmail_$TRAVIS_BRANCH"
-
-echo $STACK_NAME
-echo $DNS_HOSTNAME
-echo $DNS_DOMAIN
-echo $TRAVIS_BRANCH
-echo $SysopsEmail
-echo $AwsKeyUpload
-echo $AppDeployBucket
-
-# deploy
 aws cloudformation update-stack \
 --stack-name $STACK_NAME \
 --capabilities CAPABILITY_NAMED_IAM \
 --template-body file://cf_templates/eb_bridgepf.yml \
 --parameters \
-ParameterKey=AppDeployBucket,ParameterValue=$AppDeployBucket \
+ParameterKey=AppDeployBucket,ParameterValue=$AppDeployBucket_develop \
 ParameterKey=AppHealthcheckUrl,ParameterValue='HTTP:80/?study=api' \
 ParameterKey=AuthCreateMysqlAccounts,ParameterValue=true \
 ParameterKey=AuthProvider,ParameterValue=mysql \
 ParameterKey=AwsEbNotificationEndpoint,ParameterValue=khai.do@sagebase.org \
 ParameterKey=AwsKey,ParameterValue=$AwsKey \
-ParameterKey=AwsKeyUpload,ParameterValue=$AwsKeyUpload \
-ParameterKey=AwsKeyUploadCms,ParameterValue=$AwsKeyUploadCms \
+ParameterKey=AwsKeyUpload,ParameterValue=$AwsKeyUpload_develop \
+ParameterKey=AwsKeyUploadCms,ParameterValue=$AwsKeyUploadCms_develop \
 ParameterKey=AwsSecretKey,ParameterValue=$AwsSecretKey \
-ParameterKey=AwsSecretKeyConsents,ParameterValue=$AwsSecretKeyConsents \
-ParameterKey=AwsSecretKeyUpload,ParameterValue=$AwsSecretKeyUpload \
-ParameterKey=AwsSecretKeyUploadCms,ParameterValue=$AwsSecretKeyUploadCms \
-ParameterKey=AwsSnsKey,ParameterValue=$AwsSnsKey \
-ParameterKey=AwsSnsSecretKey,ParameterValue=$AwsSnsSecretKey \
-ParameterKey=BridgeEnv,ParameterValue=$BridgeEnv \
-ParameterKey=BridgeHealthcodeKey,ParameterValue=$BridgeHealthcodeKey \
+ParameterKey=AwsSecretKeyConsents,ParameterValue=$AwsSecretKeyConsents_develop \
+ParameterKey=AwsSecretKeyUpload,ParameterValue=$AwsSecretKeyUpload_develop \
+ParameterKey=AwsSecretKeyUploadCms,ParameterValue=$AwsSecretKeyUploadCms_develop \
+ParameterKey=AwsSnsKey,ParameterValue=$AwsSnsKey_develop \
+ParameterKey=AwsSnsSecretKey,ParameterValue=$AwsSnsSecretKey_develop \
+ParameterKey=BridgeEnv,ParameterValue=$BridgeEnv_develop \
+ParameterKey=BridgeHealthcodeKey,ParameterValue=$BridgeHealthcodeKey_develop \
 ParameterKey=BridgeUser,ParameterValue=heroku \
 ParameterKey=DNSHostname,ParameterValue=$DNS_HOSTNAME \
 ParameterKey=DNSDomain,ParameterValue=$DNS_DOMAIN \
 ParameterKey=EC2InstanceType,ParameterValue=t2.micro \
 ParameterKey=ElastiCacheInstanceType,ParameterValue=cache.t2.micro \
-ParameterKey=EmailUnsubscribeToken,ParameterValue=$EmailUnsubscribeToken \
-ParameterKey=EnterpriseStormpathHref,ParameterValue=$EnterpriseStormpathHref \
-ParameterKey=EnterpriseStormpathId,ParameterValue=$EnterpriseStormpathId \
-ParameterKey=EnterpriseStormpathSecret,ParameterValue=$EnterpriseStormpathSecret \
-ParameterKey=HibernateConnectionPassword,ParameterValue=$HibernateConnectionPassword \
-ParameterKey=HibernateConnectionUrl,ParameterValue=$HibernateConnectionUrl \
-ParameterKey=HibernateConnectionUsername,ParameterValue=$HibernateConnectionUsername \
+ParameterKey=EmailUnsubscribeToken,ParameterValue=$EmailUnsubscribeToken_develop \
+ParameterKey=EnterpriseStormpathHref,ParameterValue=$EnterpriseStormpathHref_develop \
+ParameterKey=EnterpriseStormpathId,ParameterValue=$EnterpriseStormpathId_develop \
+ParameterKey=EnterpriseStormpathSecret,ParameterValue=$EnterpriseStormpathSecret_develop \
+ParameterKey=HibernateConnectionPassword,ParameterValue=$HibernateConnectionPassword_develop \
+ParameterKey=HibernateConnectionUrl,ParameterValue=$HibernateConnectionUrl_develop \
+ParameterKey=HibernateConnectionUsername,ParameterValue=$HibernateConnectionUsername_develop \
 ParameterKey=HibernateConnectionUsessl,ParameterValue=true \
 ParameterKey=HostPostfix,ParameterValue=-$DNS_HOSTNAME.$DNS_DOMAIN \
 ParameterKey=JavaOpts,ParameterValue='-Dnewrelic.config.file=/var/app/current/newrelic/newrelic.yml -javaagent:/var/app/current/lib/com.newrelic.agent.java.newrelic-agent-3.32.0.jar' \
 ParameterKey=NewRelicAppName,ParameterValue=$DNS_HOSTNAME.$DNS_DOMAIN \
 ParameterKey=NewRelicLicenseKey,ParameterValue=$NewRelicLicenseKey \
 ParameterKey=NewRelicLog,ParameterValue=stdout \
-ParameterKey=RedisUrl,ParameterValue=$RedisUrl \
-ParameterKey=SSLCertArn,ParameterValue=$SSLCertArn \
-ParameterKey=SynapseApiKey,ParameterValue=$SynapseApiKey \
-ParameterKey=SynapseUser,ParameterValue=$SynapseUser \
-ParameterKey=SysopsEmail,ParameterValue=$SysopsEmail \
+ParameterKey=RedisUrl,ParameterValue=$RedisUrl_develop \
+ParameterKey=SSLCertArn,ParameterValue=$SSLCertArn_develop \
+ParameterKey=SynapseApiKey,ParameterValue=$SynapseApiKey_develop \
+ParameterKey=SynapseUser,ParameterValue=$SynapseUser_develop \
+ParameterKey=SysopsEmail,ParameterValue=$SysopsEmail_develop \
 ParameterKey=WebservicesUrl,ParameterValue=https://$DNS_HOSTNAME.$DNS_DOMAIN
