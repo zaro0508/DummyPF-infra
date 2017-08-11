cab94c3e5d3bbefd4d5dbc8bc24e07b43e64a163
Khai Do <zaro0508@gmail.com>
2017-08-10 21:41:02 -0700

    change output to display the Elasticache Url

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -6,12 +6,11 @@ aws cloudformation validate-template --template-body file://cf_templates/eb_brid
 
 # Handle message that shouldn't be an error, https://github.com/hashicorp/terraform/issues/5653
 echo "Deploy cloudformation templates..."
-./update_cf_stack.sh
-#message=$(./update_cf_stack.sh 2>&1 1>/dev/null)
-#error_code=$(echo $?)
-#if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
-#  echo "There are no changes that require a stack update."
-#  exit 0
-#fi
-#echo $message
-#exit $error_code
+message=$(./update_cf_stack.sh 2>&1 1>/dev/null)
+error_code=$(echo $?)
+if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
+  echo "No stack changes detected. An update is not required."
+  exit 0
+fi
+echo $message
+exit $error_code
