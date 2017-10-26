70e1c32a2d6c003e90995630e511eecb6cbcf0c3
Khai Do <zaro0508@gmail.com>
2017-10-26 14:06:26 -0700

    Switch BridgePF to use the new service user
    The Bridge app uses the AWS heroku account to access AWS resources.
    The problem is that the heroku account has too much access.  We want
    to enforce a least privilege model of access.
    
    Switch BridgePF to use the CF managed BridgePF service user account
    to access AWS resources.  This user has less privileges than heroku
    and is setup with access policies specifically for BridgePF which is
    more secure.
    
    Additional benefits:
    
    * The management of the group, users and policies are fully automated and
    changes to them are reviewable in git.
    * Makes CF scripts simpler because we no longer need to pass in keys to
    the template.  The access keys are completely managed in CF.
    * Makes AWS IAM easier to manage because BridgePF only needs one service
    account now.
    * Potentially makes BridgePF simpler since it only needs one user account
    to access only the AWS resources it needs.  It's easier to manage one
    account than five accounts.
    
    Optional todo:
    * Refactor BridgePF to only load and use one key instaed of five keys.

--- update_cf_stack.sh
+++ update_cf_stack.sh
@@ -6,13 +6,7 @@ eval export "AttachmentBucket=\$AttachmentBucket_$TRAVIS_BRANCH"
 eval export "AwsAutoScalingGroupName=\$AwsAutoScalingGroupName_$TRAVIS_BRANCH"
 eval export "AwsAutoScalingMaxSize=\$AwsAutoScalingMaxSize_$TRAVIS_BRANCH"
 eval export "AwsAutoScalingMinSize=\$AwsAutoScalingMinSize_$TRAVIS_BRANCH"
-eval export "AwsKeyUpload=\$AwsKeyUpload_$TRAVIS_BRANCH"
-eval export "AwsKeyConsents=\$AwsKeyConsents_$TRAVIS_BRANCH"
-eval export "AwsKeyUploadCms=\$AwsKeyUploadCms_$TRAVIS_BRANCH"
 eval export "AwsLoadBalancerName=\$AwsLoadBalancerName_$TRAVIS_BRANCH"
-eval export "AwsSecretKeyConsents=\$AwsSecretKeyConsents_$TRAVIS_BRANCH"
-eval export "AwsSecretKeyUpload=\$AwsSecretKeyUpload_$TRAVIS_BRANCH"
-eval export "AwsSecretKeyUploadCms=\$AwsSecretKeyUploadCms_$TRAVIS_BRANCH"
 eval export "AwsSnsNotificationEndpoint=\$AwsSnsNotificationEndpoint_$TRAVIS_BRANCH"
 eval export "BridgeEnv=\$BridgeEnv_$TRAVIS_BRANCH"
 eval export "BridgeHealthcodeRedisKey=\$BridgeHealthcodeRedisKey_$TRAVIS_BRANCH"
@@ -21,8 +15,7 @@ eval export "EmailUnsubscribeToken=\$EmailUnsubscribeToken_$TRAVIS_BRANCH"
 eval export "HibernateConnectionPassword=\$HibernateConnectionPassword_$TRAVIS_BRANCH"
 eval export "HibernateConnectionUrl=\$HibernateConnectionUrl_$TRAVIS_BRANCH"
 eval export "HibernateConnectionUsername=\$HibernateConnectionUsername_$TRAVIS_BRANCH"
-eval export "SnsKey=\$SnsKey_$TRAVIS_BRANCH"
-eval export "SnsSecretKey=\$SnsSecretKey_$TRAVIS_BRANCH"
+eval export "RedisCloudUrl=\$RedisCloudUrl_$TRAVIS_BRANCH"
 eval export "SSLCertArn=\$SSLCertArn_$TRAVIS_BRANCH"
 eval export "SynapseApiKey=\$SynapseApiKey_$TRAVIS_BRANCH"
 eval export "SynapseUser=\$SynapseUser_$TRAVIS_BRANCH"
@@ -48,15 +41,7 @@ ParameterKey=AwsAutoScalingMaxSize,ParameterValue=$AwsAutoScalingMaxSize \
 ParameterKey=AwsAutoScalingMinSize,ParameterValue=$AwsAutoScalingMinSize \
 ParameterKey=AwsDefaultVpcId,ParameterValue=$AwsDefaultVpcId \
 ParameterKey=AwsEbHealthReportingSystem,ParameterValue=enhanced \
-ParameterKey=AwsKey,ParameterValue=$AwsKey \
-ParameterKey=AwsKeyConsents,ParameterValue=$AwsKeyConsents \
-ParameterKey=AwsKeyUpload,ParameterValue=$AwsKeyUpload \
-ParameterKey=AwsKeyUploadCms,ParameterValue=$AwsKeyUploadCms \
 ParameterKey=AwsLoadBalancerName,ParameterValue=$AwsLoadBalancerName \
-ParameterKey=AwsSecretKey,ParameterValue=$AwsSecretKey \
-ParameterKey=AwsSecretKeyConsents,ParameterValue=$AwsSecretKeyConsents \
-ParameterKey=AwsSecretKeyUpload,ParameterValue=$AwsSecretKeyUpload \
-ParameterKey=AwsSecretKeyUploadCms,ParameterValue=$AwsSecretKeyUploadCms \
 ParameterKey=AwsSnsNotificationEndpoint,ParameterValue=$AwsSnsNotificationEndpoint \
 ParameterKey=BridgeEnv,ParameterValue=$BridgeEnv \
 ParameterKey=BridgeHealthcodeRedisKey,ParameterValue=$BridgeHealthcodeRedisKey \
@@ -75,8 +60,7 @@ ParameterKey=HostPostfix,ParameterValue=$HostPostfix \
 ParameterKey=JavaOpts,ParameterValue='-Dnewrelic.config.file=/var/app/current/newrelic/newrelic.yml -javaagent:/usr/local/lib/newrelic/com.newrelic.agent.java.newrelic-agent.jar' \
 ParameterKey=NewRelicAppName,ParameterValue=$DNS_HOSTNAME \
 ParameterKey=NewRelicLicenseKey,ParameterValue=$NewRelicLicenseKey \
-ParameterKey=SnsKey,ParameterValue=$SnsKey \
-ParameterKey=SnsSecretKey,ParameterValue=$SnsSecretKey \
+ParameterKey=RedisCloudUrl,ParameterValue=$RedisCloudUrl \
 ParameterKey=SSLCertArn,ParameterValue=$SSLCertArn \
 ParameterKey=SynapseApiKey,ParameterValue=$SynapseApiKey \
 ParameterKey=SynapseUser,ParameterValue=$SynapseUser \
